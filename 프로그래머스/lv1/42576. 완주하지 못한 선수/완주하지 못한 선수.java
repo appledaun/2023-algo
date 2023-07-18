cadca13e69d00d3b33e0698da4161c36651018fd
daeunK <appledaun@naver.com>
2023-07-18 17:14:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.44 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,9 +1,10 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
+        /*
         Arrays.sort(participant, 0, participant.length);
         Arrays.sort(completion, 0, completion.length);
         
@@ -14,6 +15,29 @@ class Solution {
             }
         }
         answer = participant[i];
+        */
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < completion.length; i++) {
+            if (map.containsKey(completion[i])) {
+                map.put(completion[i], map.get(completion[i]) + 1);
+            } else {
+                map.put(completion[i], 1);
+            }
+        }
+
+        for(int i = 0; i < participant.length; i++) {
+            if (map.containsKey(participant[i])) {
+                if(map.get(participant[i]) == 1) {
+                    map.remove(participant[i]);
+                } else {
+                    map.put(participant[i], map.get(participant[i]) - 1);
+                }
+            } else {
+                answer = participant[i];
+                break;
+            }
+        }
+        
         return answer;
     }
 }
